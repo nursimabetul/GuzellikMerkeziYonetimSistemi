8848090ead1199d2a13cc6a7a16e06622552cc78
Nursima Betül BALLİ <nursimabetulballi@gmail.com>
2024-12-21 22:43:07 +0300

    Randevu tanımı

---
+++ Models/Randevu.cs
@@ -0,0 +1,20 @@
+﻿namespace GuzellikMerkeziYonetimSistemi.Models
+{
+	public class Randevu
+	{
+		public int Id { get; set; }
+		public DateTime RandevuTarihi { get; set; }
+		
+		// İşlem Türü
+
+		// İşlem Süresi
+
+		// Ücret
+
+		// Randevu alan kullanıcı bilgileri
+
+		// Durumu
+		public string Aciklama { get; set; }
+
+	}
+}
